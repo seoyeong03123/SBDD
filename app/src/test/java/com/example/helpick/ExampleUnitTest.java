4ab0b4740b52e8d07f4a1772bab9846ee1f1023d
yeong <seoyeong03123>
2020-11-03 18:56:41 +0900

    commit

--- app/src/test/java/com/example/helpick/ExampleUnitTest.java
+++ app/src/test/java/com/example/helpick/ExampleUnitTest.java
@@ -13,5 +13,10 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+
+    }
+
+    public void test(){
+        int sdf [6][6]
     }
 }
